09eff387619632ca7b8464f33a171a2c1b99c5e9
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-11-19 01:22:40 -0800

    Improving the user interface and displaying the name of the message sender along with the sending time

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,28 @@
 const socket = io();
 const form = document.getElementById('chat-form');
+const chatMessageDiv = document.querySelector('.chat-messages');
 
-socket.on("message", (data) => outputChatMessage(data))
+socket.on("message", (data) => {
+    outputChatMessage(data);
+    // اسکرول شدن به آخرین پیام ارسالی
+    chatMessageDiv.scrollTop = chatMessageDiv.scrollHeight;
+} )
 
 form.addEventListener("submit",(event) => {
     event.preventDefault();
     const message = event.target.msg.value;
     socket.emit("ChatMessage",message);
+    event.target.msg.value = '';
+    event.target.msg.focus();
 })
 
 // نمایش پیام ارسالی کاربر
 function outputChatMessage(chatMessage) {
     const div = document.createElement("div");
     div.classList.add("message");
-    div.innerHTML = `<p class="meta">محمد <span>9:12pm</span></p>
+    div.innerHTML = `<p class="meta"> ${chatMessage.username}<span> ${chatMessage.time} </span></p>
     <p class="text">
-       ${chatMessage}
+       ${chatMessage.text}
     </p>`;
     document.querySelector('.chat-messages').appendChild(div);
 }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const socket = require('socket.io');
 const path = require('path');
 const http = require('http');
+const messageHandler = require('./utils/chat_message_handeler');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,12 +12,12 @@ app.use(express.static(path.join(__dirname, "public")));
 
 //connection => یک ایونت از قبل تعریف شده هست
 io.on("connection", (socket) => {
-    socket.emit('message',"user join the chat server")
+    socket.emit('message',messageHandler("mohammad","user join the chat server"))
     //broadcast => پیام برای تمام کاربران به حاضر در چت به غیر از کاربر جاری ارسال میشه
-    socket.broadcast.emit('message',"user join the chat server");
+    //socket.broadcast.emit('message',"user join the chat server");
 
     socket.on("ChatMessage", (message) =>{
-        io.emit('message', message);
+        io.emit('message',messageHandler("mohammad",message));
     })
 })
 
