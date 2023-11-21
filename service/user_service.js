9e715f90a438293a6594b7a18db9c3b22bff3f89
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-11-20 23:29:17 -0800

    impl user service

---
+++ service/user_service.js
@@ -0,0 +1,15 @@
+const users = [];
+
+function joinUser(id,username,room){
+    const user = {id,username,room};
+    users.push(user);
+    return user;
+}
+
+function getCurrentUser(id) {
+  users.find(user => user.id === id);
+}
+
+module.exports = {
+    joinUser,getCurrentUser
+}
